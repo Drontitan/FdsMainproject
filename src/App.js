62bde33c998ec49627fa2e130bda9f71ae3e4b34
Ajinkyap331 <ajinkyap331@gmail.com>
2022-01-10 20:33:32 +0530

    Added React Router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { Calender } from "./Components/Calender";
+import { Login } from "./Components/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/calender">
+          <Calender />
+        </Route>
+        <Route exact path="/">
+          <>
+            This is Home Page
+          </>
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Components/Calender.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Calender = () => {
+    return (
+        <div>
+            This is Calender
+        </div>
+    )
+}
